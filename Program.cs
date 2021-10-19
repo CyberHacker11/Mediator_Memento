58742453a371c9a97867874afa9b693d528cd97c
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-10-19 17:11:19 +0400

    upp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mediator_Memento;
+using System;
 
 namespace Mediator_Memento
 {
@@ -116,10 +117,19 @@ namespace Mediator_Memento
 
 
 class Program
+{
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Originator o = new Originator();
+        o.State = "On";
+        // Store internal state
+        Caretaker c = new Caretaker();
+        c.Memento = o.CreateMemento();
+        // Continue changing originator
+        o.State = "Off";
+        // Restore saved state
+        o.SetMemento(c.Memento);
+        // Wait for user
+        Console.ReadKey();
     }
 }
